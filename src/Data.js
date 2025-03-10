178e60686ab537148dbe2222bc320ef8b3a7b327
Mac044 <Olasubomiagbomeji@gmail.com>
2025-03-10 08:56:30 -0700

    Data and Datas

---
+++ src/Data.js
@@ -0,0 +1,62 @@
+const information = [
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741616748/conference_svgrepo.com_poina9.png",
+        title: "Keynote Speakers",
+        info: "Hear from renowned speakers and industry leaders delivering inspirational talks.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741616726/Vector_nbojij.png",
+        title: "Scholarship",
+        info: "Announcement of 1,000 scholarships for both coding and non-coding tech skills",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741616818/network-group_svgrepo.com_x1lujd.png",
+        title: "Networking Sessions",
+        info: "Network with 5,000+ attendees from diverse experiences and connect with industry professionals, mentors, and peers.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618772/XMLID_238__tozjh6.png",
+        title: "Pre Confrence MeetUps",
+        info: "A tour across various local government in Lagos",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618770/Group_vyedr4.png",
+        title: "Demo Sessions and Product Exhibitions",
+        info: "Experience the latest tech innovations through demos and product exhibitions.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618772/reception-person-people-demo_svgrepo.com_ec5xbh.png",
+        title: "Tech Talks and Lighting Talks",
+        info: "Attend short, impactful presentations on various tech topics.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618769/Group_1_gurh05.png",
+        title: "Interactive Q&A Sessions",
+        info: "Participate in Q&A sessions to deepen your understanding of the tech space",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618769/Group_2_mxhjc8.png",
+        title: "Entertainment and Recreational Activities",
+        info: "Enjoy various entertainment and recreational activities throughout the event.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618770/Group_3_sjcprz.png",
+        title: "Career Fair",
+        info: "Explore career opportunities and meet potential employers.",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618770/group-discussion-meetingx3_svgrepo.com_qwkarv.png",
+        title: "Panel Discussions",
+        info: "Engage in discussions on current trends and future directions in the tech industry",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741618771/_x35_62_x2C__competition_x2C__contest_x2C__expert_x2C__judge_x2C__jury_wsvba1.png",
+        title: "Hackathons and StartUp Pitch Competitions",
+        info: "Compete in hackathons and pitch competitions to showcase your skills and also to solve real-world problems through technology and innovation",
+    },
+    {
+        icon: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741616726/Vector_nbojij.png",
+        title: "Workshop",
+        info: "Participate in hands-on sessions covering a range of tech skills, including coding, cybersecurity, digital marketing, and blockchain technology",
+    },
+]
---
+++ src/Datas.js
@@ -0,0 +1,62 @@
+const People = [
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621994/Ellipse_10_obgowk.png",
+        name: "Liam O'Connor",
+        work: "Smart Contract Engineer",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621593/Frame_175_rvnair.png",
+        name: "Diego Ruiz",
+        work: "CryptoCurrency Exchange Platform Developer ",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621595/Frame_176_wcxma5.png",
+        name: "Zain Malik",
+        work: "Decentralized Finance (DeFi) Product Manger",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621596/Frame_177_umuiqx.png",
+        name: "Evelyn Frost",
+        work: "Blockchain Security Auditor",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621592/Frame_174_ayfytm.png",
+        name: "Yuki Tanaka",
+        work: "Non-Fungible Token(NFT) Marketplace Curator",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621592/Frame_175_1_eow7na.png",
+        name: "Rashid Al-Fayed",
+        work: "Blockchain Scalability Solutions Architect",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621593/Frame_176_1_pze8cx.png",
+        name: "Olabanji David",
+        work: "UX Designer",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621595/Frame_177_1_k95s4n.png",
+        name: "Aria Patel",
+        work: "Metaverse Experience Designer",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621596/Frame_174_1_ppg8rb.png",
+        name: "Felix Nguyen",
+        work: "Crypto Mining Operations Manage",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621593/Frame_175_2_ew1zkm.png",
+        name: "Zoe Blackwell",
+        work: "Tokenomics Strategist",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621593/Frame_176_2_tn395l.png",
+        name: "Mateo Salazar",
+        work: "Decentralized Identity Systems Developer",
+    },
+    {
+        images: "https://res.cloudinary.com/doawzlqxi/image/upload/v1741621597/Frame_177_2_mz5s95.png",
+        name: "Naomi Chen",
+        work: "Quantum Cryptography Researcher",
+    },
+]
